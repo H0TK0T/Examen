6f70c01f3b947c8d6fd773698fed20a892f75f89
H0TK0T <159611155+H0TK0T@users.noreply.github.com>
2024-06-29 20:27:33 +0000

    Done code

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -8,18 +8,21 @@ const MainForm = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [email, setEmail] = useState('');
   const [futureDate, setFutureDate] = useState('');
+  const [time, setTime] = useState('');
   const [fullNameError, setFullNameError] = useState('');
   const [dateOfBirthError, setDateOfBirthError] = useState('');
   const [phoneNumberError, setPhoneNumberError] = useState('');
   const [emailError, setEmailError] = useState('');
   const [futureDateError, setFutureDateError] = useState('');
+  const [timeError, setTimeError] = useState('');
+  const [showFormData, setShowFormData] = useState(false);
 
   const handleFullNameChange = (value) => {
     setFullName(value);
     if (value.trim() === '') {
       setFullNameError('ФИО не может быть пустым');
     } else if (!validateFullName(value)) {
-      setFullNameError('Пожалуйста, введите корректное ФИО (хотя бы два слова кириллицей)');
+      setFullNameError('Пожалуйста, введите корректное ФИО');
     } else {
       setFullNameError('');
     }
@@ -29,6 +32,8 @@ const MainForm = () => {
     setDateOfBirth(value);
     if (value.trim() === '') {
       setDateOfBirthError('Дата рождения не может быть пустой');
+    } else if (!validateDateOfBirth(value)) {
+      setDateOfBirthError('Дата рождения не может быть больше 2006 года');
     } else {
       setDateOfBirthError('');
     }
@@ -77,6 +82,15 @@ const MainForm = () => {
     }
   };
 
+  const handleTimeChange = (value) => {
+    setTime(value);
+    if (value.trim() === '') {
+      setTimeError('Выберите удобное вам время посещения');
+    } else {
+      setTimeError('');
+    }
+  };
+
   const handleBlur = (e) => {
     const { id, value } = e.target;
     switch (id) {
@@ -102,13 +116,21 @@ const MainForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Дополнительная проверка перед отправкой формы, если нужно
+    const formData = {
+      fullName: fullName,
+      dateOfBirth: dateOfBirth,
+      phoneNumber: phoneNumber,
+      email: email,
+      futureDate: futureDate,
+      time: time,
+    };
+    setShowFormData(true);
   };
 
   return (
     <main className="main">
       <form onSubmit={handleSubmit} className="form">
-        <h1>Название формы</h1>
+        <h3>Запись на мед. услуги МедВед</h3>
         <div className="formGroup">
           <input
             type="text"
@@ -116,13 +138,14 @@ const MainForm = () => {
             value={fullName}
             onChange={(e) => handleFullNameChange(e.target.value)}
             onBlur={handleBlur}
-            placeholder="Иван Иванов"
+            placeholder="Иван Иванов Иванович"
             required
           />
           <label htmlFor="fullName" className="label sr-only">ФИО</label>
           {fullNameError && <p className="error">{fullNameError}</p>}
         </div>
         <div className="formGroup">
+        <small>Напишите вашу дату рождения:</small>
           <input
             type="date"
             id="dateOfBirth"
@@ -142,8 +165,9 @@ const MainForm = () => {
             value={phoneNumber}
             onChange={(e) => handlePhoneNumberChange(e.target.value)}
             onBlur={handleBlur}
-            placeholder="+7-900-000-00-00"
+            placeholder="+7(900)-000-00-00"
             required
+            maxLength={15}
           />
           <label htmlFor="phoneNumber" className="label sr-only">Номер телефона</label>
           {phoneNumberError && <p className="error">{phoneNumberError}</p>}
@@ -162,6 +186,7 @@ const MainForm = () => {
           {emailError && <p className="error">{emailError}</p>}
         </div>
         <div className="formGroup">
+        <small>Выберите дату посещения:</small>
           <input
             type="date"
             id="futureDate"
@@ -173,10 +198,52 @@ const MainForm = () => {
           <label htmlFor="futureDate" className="label sr-only">Будущая дата</label>
           {futureDateError && <p className="error">{futureDateError}</p>}
         </div>
+        <div className="formGroup">
+        <small>Выберите время посещения:</small>
+          <select
+            id="time"
+            value={time}
+            onChange={(e) => handleTimeChange(e.target.value)}
+            onBlur={handleBlur}
+            required
+            defaultValue="08:00"
+          >
+            <option value="08:00">8:00</option>
+            <option value="09:00">9:00</option>
+            <option value="10:00">10:00</option>
+            <option value="11:00">11:00</option>
+            <option value="12:00">12:00</option>
+            <option value="13:00">13:00</option>
+            <option value="14:00">14:00</option>
+            <option value="15:00">15:00</option>
+            <option value="16:00">16:00</option>
+            <option value="17:00">17:00</option>
+            <option value="18:00">18:00</option>
+            <option value="19:00">19:00</option>
+            <option value="20:00">20:00</option>
+            <option value="21:00">21:00</option>
+            </select>
+            <label htmlFor="time" className="label sr-only">Время</label>
+            {timeError && <p className="error">{timeError}</p>}
+        </div>
         <button type="submit">Отправить</button>
       </form>
+      {showFormData && (
+        <div className="formDataContainer" style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', zIndex: 1 }}>
+          <div className="formData" style={{ backgroundColor: '#2B4281', padding: 20, borderRadius: 10, boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)' }}>
+            <h2>Form Data</h2>
+            <ul>
+              <li>ФИО: {fullName}</li>
+              <li>Дата рождения: {dateOfBirth}</li>
+              <li>Номер телефона: {phoneNumber}</li>
+              <li>Электронная почта: {email}</li>
+              <li>Дата посещения: {futureDate}</li>
+              <li>Время посещения: {time}</li>
+            </ul>
+          </div>
+        </div>
+      )}
     </main>
   );
 };
-
 export default MainForm;
--- src/components/Form/utils.js
+++ src/components/Form/utils.js
@@ -1,39 +1,45 @@
 // проверка на ввод имени
 const validateFullName = (fullName) => {
-    // проверка на наличие хотя бы двух слов кириллицей
-    const regex = /^[А-Яа-яЁё]+\s[А-Яа-яЁё]+(\s[А-Яа-яЁё]+)?$/;
-  
-    return regex.test(fullName);
-  };  
-  
+  // проверка на наличие трёх слов кириллицей
+  const regex = /^[А-Яа-яЁё]+\s[А-Яа-яЁё]+\s[А-Яа-яЁё]+?$/;
 
-  const validatePhoneNumber = (phoneNumber) => {
-    const cleaned = phoneNumber.replace(/\D/g, '');
-    return /^(?:\+7|8)\d{10}$/.test(cleaned);
-  };
-    
-  const validateEmail = (email) => {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailRegex.test(email);
-  };
-  
-  const validateFutureDate = (date) => {
-    const selectedDate = new Date(date);
-    const currentDate = new Date();
+  return regex.test(fullName);
+};  
 
-    selectedDate.setHours(0, 0, 0, 0);
-    currentDate.setHours(0, 0, 0, 0);
+
+const validatePhoneNumber = (phoneNumber) => {
+  const cleaned = phoneNumber.replace(/\D/g, '');
+  return /^(?:\+7|8)\d{10}$/.test(cleaned);
+};
   
-    const nextDay = new Date(currentDate);
-    nextDay.setDate(currentDate.getDate() + 1);
-  
-    return selectedDate >= nextDay;
-  };
-    
-  
+const validateEmail = (email) => {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+};
+
+const validateFutureDate = (date) => {
+  const selectedDate = new Date(date);
+  const currentDate = new Date();
+
+  selectedDate.setHours(0, 0, 0, 0);
+  currentDate.setHours(0, 0, 0, 0);
+
+  const nextDay = new Date(currentDate);
+  nextDay.setDate(currentDate.getDate() + 1);
+
+  return selectedDate >= nextDay;
+};
+
+const validateDateOfBirth = (date) => {
+  const selectedDate = new Date(date);
+  const maxDate = new Date('2006-12-31');
+
+  return selectedDate <= maxDate;
+};
+
 export {
-    validateFullName, 
-    validateEmail,
-    validateFutureDate
+  validateFullName, 
+  validateEmail,
+  validateFutureDate,
+  validateDateOfBirth
 }
-      
